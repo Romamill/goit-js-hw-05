21a93e4c026110eb8d0293390679a463141d1f94
Romamill <122994080+Romamill@users.noreply.github.com>
2023-05-21 18:48:57 +0300

    task2

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -6,3 +6,14 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const ulEl = document.getElementById('ingredients');
+
+const liElements = ingredients.map((ingredient) => {
+  const liEl = document.createElement('li');
+  liEl.textContent = ingredient;
+  liEl.classList.add('item');
+  return liEl;
+}); 
+
+ulEl.append(...liElements);
