1d63446c9f51996aa72cb53875ba6c54c988aa87
Romamill <122994080+Romamill@users.noreply.github.com>
2023-05-22 18:23:38 +0300

    task9

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -3,3 +3,16 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const bodyEl = document.querySelector('body');
+
+const swapBtnEl = document.querySelector('.change-color');
+
+const colorEl = document.querySelector('.color');
+
+swapBtnEl.addEventListener('click', () => {
+  const randomColor = getRandomHexColor();
+  document.body.style.backgroundColor = randomColor;
+  colorEl.textContent = randomColor;
+
+});
